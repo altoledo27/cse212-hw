c60e092a8a51df252f6476d46af1a52457b4f3d3
altoledo27 <altoledoz27@gmail.com>
2026-07-11 11:34:09 -0600

    week1 code problems

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+
 public static class Arrays
 {
     /// <summary>
@@ -12,8 +15,19 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // Plan: 
+        // 1.- Recieve the value of number and length. Create the array where is going to be saved the numbers with the length of lenght. 
+        // 2.- Using a for loop, with the variable (could be i or any letter) i starting on 1 (because using 0 could return a 0 and we don't want this), the second part of the parameters of the for loop should be i<= length or  i< length + 1, in order to have just the right number of multiples we want to get. i++ for the last parameter to move to the other number that would multiply the desire number.
+        // 3.- The number should be multiplied with the value of i, and this value have to be saved on the array, I will call it 'multiples' and the position where is going to be saved the value of that interation has to be -1 of the value of i, because whe should start on the 0 position of the array .
+        // 4.- Finally, return the array.
 
-        return []; // replace this return statement with your own
+        double[] multiplies = new double[length];
+        for (var i = 1; i <= length; i++)
+        {
+           multiplies[i-1]= number * i;
+        }
+
+        return multiplies; 
     }
 
     /// <summary>
@@ -29,5 +43,22 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        //MY PLAN:
+            //Create a copy of the original list
+            //Iterate through the copy list
+            //We are going to add the numbers from data to our new list but with the correct posicion after doing the rotation. To do this , we need to calculate the new position like this: 
+                // newPosition = ( current posicion + amount) % originalList.Count;
+            //Then assign this to the new list:
+                //originalList[newPosition] = copyList[current Position];
+        //Implementation:
+        //Copy of the original list       
+        List<int> chida = new List<int>(data);
+        for(int i = 0; i < data.Count; i++)
+        {
+            //Calculating the new position. 
+            int newPosition = (i + amount) % data.Count;
+            //Changing the originalList
+            data[newPosition] = chida[i];
+        }
     }
 }
